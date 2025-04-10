6a72479250f463c8d6712fd8726fca371c6967e7
Vladislav-cpp <105502335+Vladislav-cpp@users.noreply.github.com>
2025-04-10 21:28:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
 	std::cout << "=============================================" << std::endl << std::endl;
 
 	//● зчитувати масиви з файлу
-    auto arrays = miscUtils::ReadArraysFromFile(s_sInputFileName);
+	auto arrays = miscUtils::ReadArraysFromFile(s_sInputFileName);
 
 	//● друкувати вміст масивів
 	std::cout << "PrintArrays" << std::endl;
@@ -21,26 +21,26 @@ int main() {
 	std::cout << std::endl;
 
 	//● сортувати масиви без допомоги вбудованих функцій
-    auto transformer = TransformerFactory::Create(Transformation::sort);
+	auto transformer = TransformerFactory::Create(Transformation::sort);
 	transformer->Transform(arrays);
 
 	//● знаходити перетин всіх трьох масивів
 	transformer = TransformerFactory::Create(Transformation::intersect);
-    transformer->Transform(arrays);
+	transformer->Transform(arrays);
 
 	miscUtils::QuickSort(arrays.begin(), arrays.end(), 
-						std::function<bool(const std::vector<int>& a,const std::vector<int>& b)>{[](const auto& a, const auto& b) {
-							 return a.size() > b.size();
-						}});
+			std::function<bool(const std::vector<int>& a,const std::vector<int>& b)>{[](const auto& a, const auto& b) {
+				 return a.size() > b.size();
+			}});
 
 	//● знаходити перетин двох масивів з найбільшою найбільжиною
 	auto twoLongestArray = std::vector<std::vector<int>>{ arrays[0], arrays[1] };
 	transformer = TransformerFactory::Create(Transformation::intersect);
-    transformer->Transform(twoLongestArray);
+	transformer->Transform(twoLongestArray);
 
 	//● створювати відсортований у зворотному порядку масив, що містить лише унікальні елементи з трьох представлених масивів
 	transformer = TransformerFactory::Create(Transformation::uniqueReverseSorted);
-    transformer->Transform(arrays);
+	transformer->Transform(arrays);
 
-    return 0;
+   	return 0;
 }
